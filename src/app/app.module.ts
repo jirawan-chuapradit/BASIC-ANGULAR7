89316eaf6e92f788bfed74bf485d1742cbc723d6
jirawan-chuapradit <jirawan-chuapradit@users.noreply.github.com>
2019-06-02 14:04:18 +0700

    ng add ngx-bootstrap
    ใช้ผ่าน ngx bootsstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BsDropdownModule } from 'ngx-bootstrap';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
