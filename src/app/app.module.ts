b04f8c1d17e2c6abc0ddd44f9392761fd2377564
jirawan-chuapradit <jirawan-chuapradit@users.noreply.github.com>
2019-06-03 17:04:47 +0700

    Revert "สร้าง component"
    This reverts commit 0cdf446e155e103304f29342c9d66cf7ad947b37.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,10 @@ import { BsDropdownModule } from 'ngx-bootstrap';
 
 import {FormsModule} from '@angular/forms'
 import { from } from 'rxjs';
-import { Com2Component } from './com2/com2.component';
-import { Com1Component } from './com1/com1.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Com2Component,
-    Com1Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
